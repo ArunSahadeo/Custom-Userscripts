42b37d8ed8ba36773f9cc99399d2cc8222d8dc76
Arun Sahadeo <arunjamessahadeo@gmail.com>
2019-10-05 17:36:48 +0100

    Initial commit / add userscript to skip YouTube ads

---
+++ Skip YouTube Ads.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name Skip YouTube Ads
+// @description Automatically skips YouTube ads once the Skip Ad button is available
+// @namespace 
+// @version 0.0.1
+// @match https://*.youtube.com/*
+// @run-at document-idle
+// @license MIT
+// ==/UserScript==
+
+(function (global) {
+    'use strict';
+
+    var self = global; 
+    self.requestUri = new String(window.location.pathname).replace(/^\/+/, '');
+
+    self.elementExists = function (sel) {
+        return document.querySelectorAll(sel).length;
+    };
+
+    self.toggleAdSkip = function (sel) {
+        if (self.requestUri !== 'watch') {
+            return;
+        }
+
+        var canSkipAd = setInterval(function () {
+            let skipBtn = document.querySelector('.ytp-ad-skip-button'); 
+
+            if (typeof skipBtn === 'object' && skipBtn !== null) {
+                clearInterval(canSkipAd);
+                skipBtn.click();
+            }
+        }, 1000);
+    }
+
+    self.bindEvents = function () {
+        if (self.requestUri === 'watch') {
+            self.toggleAdSkip();
+        }
+    };
+    
+    function init() {
+        self.bindEvents();
+    }
+
+    init();
+})(window);
